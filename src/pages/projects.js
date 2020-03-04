6980027b386ba375500aa467120e5842d05e3487
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-03-04 12:50:03 -0800

    add basic projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,7 +15,13 @@ const ProjectPage = () => (
   <Layout>
     <SEO title='projects'/>
     <Container>
-      
+        <BodyText><Project href="https://github.com/keolasin/grocery-todo">Grab n' Grocer</Project>An app for users to create shared grocery lists and update them in real-time</BodyText>
+        <BodyText><Project href="https://keolasin-greenlete.herokuapp.com">Greenlete</Project>An app for users to log litter they find and pick-up during a workout</BodyText>
+        <BodyText><Project href="https://github.com/keolasin/bloc-jams-react">Music Player</Project>An app for playing music</BodyText>
+        <BodyText><Project href="https://github.com/keolasin/bloc-chat-react">Chatroom</Project>A chatroom app</BodyText>
+        <BodyText><Project href="https://keolasin-bloccit.herokuapp.com/">RedditClone</Project>A place for users to share and upvote stories</BodyText>
+        <BodyText><Project href="https://keolasin-blocipedia.herokuapp.com">Wiki Clone</Project>A wiki-topic clone</BodyText>
+        <BodyText><Project href="https://repl.it/@keolasin">Repl Practice</Project>A collection of practice problems, algorithms, and data structures</BodyText>
     </Container>
   </Layout>
 )
@@ -23,17 +29,29 @@ const ProjectPage = () => (
 export default ProjectPage
 
 // css styling
-const Container = styled.section`
+const Container = styled.article`
   width: 100%;
   height: 100%;
+  max-height: 90vh;
+  align-text: left;
+  overflow: auto;
   background-color: rgba(0, 0, 0, 0.75);
+`;
+
+const Project = styled.a`
+  text-decoration: none;
+  display: inline-block;
+  margin: 5px;
+  font-size: 1.5em;
+  font-weight: bold;
+  color: #798BE4;
 `;
 
 const BodyText = styled.p`
   color: #DBE7FB;
   font-family: 'Forma DJR Display', sans-serif;
   line-height: 1.5em;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   ${mediaQuery[1]} {
       font-size: 1.8rem;
   }
