7930a0804cb1caad23cda7e2a98c3a520a00d537
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-07-07 20:54:49 -0700

    add prelim album view and query fix

---
+++ src/components/albumCover.js
@@ -0,0 +1,85 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+import { mediaQuery } from "../styles/global.js"
+
+import Layout from "../components/layout"
+
+const AlbumCover = ({ album }) => {
+
+
+    return (
+        <Container>
+            <CoverImage 
+                fluid={album.coverImg.childImageSharp.fluid}
+            />
+            <Hover>
+                <PhotoText>
+                    {album.frontmatter.title}
+                </PhotoText>
+            </Hover>
+        </Container>
+    )
+}
+
+export default AlbumCover;
+
+const Container = styled.a`
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+    border-radius: 25px;
+    box-shadow: 0 4px 8px 0 rgba(121, 139, 228, 0.2), 0 6px 20px 0 rgba(121, 139, 228, 0.15);
+    :hover {
+        box-shadow: 0 4px 8px 0 rgba(121, 139, 228, 0.4), 0 6px 20px 0 rgba(121, 139, 228, 0.35);
+    }
+    :focus {
+        box-shadow: 0 4px 8px 0 rgba(188, 150, 18, 0.4), 0 6px 20px 0 rgba(188, 150, 18, 0.35);
+    }
+    :active {
+        box-shadow: 0 4px 8px 0 rgba(188, 150, 18, 0.4), 0 6px 20px 0 rgba(188, 150, 18, 0.35);
+    }
+`;
+
+const Hover = styled.section`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: 0.5s ease;
+    background-color: black;
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const PhotoText = styled.p`
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+    line-height: 3rem;
+`;
+
+const ImageTile = styled(Img)`
+    width: 100%;
+    height: 100%;
+`;
+
+const CoverImage = styled(Img)`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
--- src/pages/albums.js
+++ src/pages/albums.js
@@ -3,102 +3,83 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import styled from "@emotion/styled"
-import { mediaQuery } from '../styles/global.js'
+import { mediaQuery } from "../styles/global.js"
 
-import Gallery from "../components/gallery.js"
-
-import Layout from "../../components/layout"
+import Layout from "../components/layout"
+import AlbumCover from "../components/albumCover"
 
 // query for the cover photos of each album, along with the name of each album to appear on the galleries page
-export const galleryCoverQuery = graphql`
+export const albumsQuery = graphql`
   query {
-    allFile(filter: {name: {glob: "*Cover"}, relativeDirectory: {glob: "galleries/*"}}) {
-      nodes {
-        name
-        childImageSharp {
-          fluid (maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp
+    allMarkdownRemark(filter: {frontmatter: {photos: {elemMatch: {title: {ne: null}}}}}) {
+      edges {
+        node {
+          frontmatter {
+            date
+            title
+            photographer
+            description
+          }
+          coverImg {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
           }
         }
-        relativeDirectory
       }
     }
   }
-`
-// first generate all the gallery names based on folders in /images/galleries
-// then use a cover photo for each of the galleries
-// use gallery photos as links to gallery/photos page
+`;
 
-const GalleriesPage = ({ data }) => {
-  // create album cover nodes
-  const covers = data.allFile.nodes;
+const GalleriesPage = ( {data: {allMarkdownRemark: {edges}}} ) => {
+  // albums
+  const Albums = edges
+    .map(edge => <AlbumCover key={edge.node.id} album={edge.node} />)
 
   return (
     <Layout>
       <Gallery>
-        {covers.map((image, index) => (
-          <Container
-            key={index}
-            href={`${image.relativeDirectory.replace('galleries/', '')}`}>
-              <AlbumImage
-                fluid={image.childImageSharp.fluid}
-                alt={image.name}
-                title={image.name}
-              />
-              <Hover>
-                <PhotoText>{image.relativeDirectory.replace('galleries/', '')}</PhotoText>
-              </Hover>
-          </Container>
-        ))}
+        {Albums}
       </Gallery>
     </Layout>
   )
 }
 
-export default GalleriesPage
+export default GalleriesPage;
 
-const Container = styled.a`
-  position: relative;
-  overflow: hidden;
-  cursor: pointer; 
-`;
-
-const Hover = styled.section`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: 0.5s ease;
-  background-color: black;
-  :hover {
-    opacity: 0.8;
+const Gallery = styled.article`
+  display: grid;
+  padding: 10px;
+  grid-gap: 1px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: minmax(125px, auto);
+  ${mediaQuery[0]} {
+    font-size: 1.8rem;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-auto-rows: minmax(250px, auto);
   }
-`;
-
-const PhotoText = styled.p`
-  color: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  text-align: center;
-  line-height: 3rem;
-`;
-
-const ImageTile = styled(Img)`
-  width: 100%;
-  height: 100%;
-`;
-
-const AlbumImage = styled(Img)`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  ${mediaQuery[1]} {
+    font-size: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(275px, 1fr));
+    grid-auto-rows: minmax(275px, auto);
+  }
+  ${mediaQuery[2]} {
+    font-size: 2.3rem;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-auto-rows: minmax(300px, auto);
+  }
+  ${mediaQuery[3]} {
+    font-size: 2.5rem;
+    grid-gap: 5px;
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    grid-auto-rows: minmax(350px, auto);
+  }
+  ${mediaQuery[4]} {
+    font-size: 3.5rem;
+    grid-gap: 5px;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    grid-auto-rows: minmax(400px, auto);
+  }
 `;
