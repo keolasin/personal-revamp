d9465b66f5e09f2fead9d090f6c790ba447c6ce4
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-08-12 21:24:34 -0700

    refactor about and contact pages css, modularize

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 import styled from "@emotion/styled"
-import { mediaQuery } from "../styles/global.js"
+import { mediaQuery, Container, BlurbImage } from "../styles/global.js"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -27,7 +27,6 @@ const IndexPage = ({ data }) => {
     <Layout>
       <SEO title='home'/>
       <Container>
-        <ToggleButton/>
         <BlurbImage
           fluid={portrait.file.childImageSharp.fluid}
           alt="Yours truly"
@@ -41,61 +40,20 @@ const IndexPage = ({ data }) => {
 export default IndexPage
 
 // css styling
-const Container = styled.section`
-  border-radius: 25px;
-  background-color: rgba(0, 0, 0, 0.75);
-  height: 100%;
-  display: grid;
-  place-items: center;
-  transition: 1s ease 0.3s;
-`;
-
-const BlurbImage = styled(Img)`
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-  margin: 10px auto;
-  ${mediaQuery[0]} {
-    width: 125px;
-    height: 125px;
-  }
-  ${mediaQuery[1]} {
-    width: 150px;
-    height: 150px;
-  }
-  ${mediaQuery[2]} {
-    width: 175px;
-    height: 175px;
-    display: flex;
-    place-items: flex-start;
-  }
-  ${mediaQuery[3]} {
-    width: 200px;
-    height: 200px;
-    display: flex;
-  }
-  ${mediaQuery[4]} {
-    width: 225px;
-    height: 225px;
-    display: flex;
-  }
-`
-
 const BodyText = styled.p`
   border-radius: 25px;
   color: #DBE7FB;
   font-family: 'acumin-pro', sans-serif;
   line-height: 1.2em;
   font-size: 1rem;
-  padding: 15px;
-  margin-bottom: 0px;
-  ${mediaQuery[1]} {
-      font-size: 1.5rem;
+  padding: 5px;
+  margin: auto 0;
+  ${mediaQuery[2]} {
+      font-size: 1.2rem;
+      margin: 10px;
   }
   ${mediaQuery[4]} {
       font-size: 1.7rem;
-      margin: 0 20px;
   }
 `;
 
