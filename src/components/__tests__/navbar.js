51d92cd97996d8322568c169dbbac2c13b4299f6
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-08-27 15:37:37 -0700

    add unit test for navbar, config

---
+++ src/components/__tests__/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import NavBar from "../navbar"
+
+describe("Navbar", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<NavBar />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
