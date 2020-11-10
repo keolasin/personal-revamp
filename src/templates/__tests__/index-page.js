7459357a364f9ac0b4a818a53ce71ecc49a301bb
Keolasin <43076895+keolasin@users.noreply.github.com>
2020-11-09 20:16:21 -0800

    adjust test mock data

--- src/templates/__tests__/index-page.js
+++ src/templates/__tests__/index-page.js
@@ -12,7 +12,14 @@ beforeEach(() => {
 	useStaticQuery.mockImplementation(() => ({
 		mobileImage,
 		desktopImage,
-        thumbnailImg
+        thumbnailImg,
+        site: {
+            siteMetadata: {
+                title: `MReyes`,
+                description: `Personal portfolio and photography site featuring the work of Matthew Reyes`,
+                author: `@keolasin`,
+            }
+        }
 	}));
 
 	const uuid = require("short-uuid");
@@ -23,14 +30,27 @@ describe("Index Page", () => {
 	// mock data
     // templateKey, title, thumbnailImg, heading, body
 	const data = {
-		indexPage: {
+		markdownRemark: {
 			html: "<p>Test body</p>",
 			frontmatter: {
 				title: "Index page",
 				heading: "Test header",
 				thumbnailImg: "testImg.jpg",
 			},
-		},
+            childrenFile: [
+                {
+                    childImageSharp: {
+                        fluid: {
+                            aspectRatio: 1,
+                            base64: "data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAUABQDASIAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQFAgP/xAAWAQEBAQAAAAAAAAAAAAAAAAAAAQL/2gAMAwEAAhADEAAAAa5NLNxv3EUxlnYP/8QAGhAAAgMBAQAAAAAAAAAAAAAAAREAAgMxIv/aAAgBAQABBQLTlStC2QxVDc3D7D6KE//EABYRAQEBAAAAAAAAAAAAAAAAAAIQMf/aAAgBAwEBPwE5DP/EABYRAQEBAAAAAAAAAAAAAAAAAAEQMf/aAAgBAgEBPwF2M//EABoQAAICAwAAAAAAAAAAAAAAAAERABACITH/2gAIAQEABj8C6oAMm73Rjr//xAAZEAEAAwEBAAAAAAAAAAAAAAABABExIRD/2gAIAQEAAT8h4OK9YL+NC35C6wM5KzyMWhBWy7Sf/9oADAMBAAIAAwAAABCwAH3/xAAYEQADAQEAAAAAAAAAAAAAAAAAARExcf/aAAgBAwEBPxCW4uGGU//EABgRAQEAAwAAAAAAAAAAAAAAAAEAEBFh/9oACAECAQE/ECgk7jov/8QAGxABAAIDAQEAAAAAAAAAAAAAAQARITFBYXH/2gAIAQEAAT8QCbVD2FOojJqTwTf2IKOAHkQKmXBYUSyqW2DgE87BJwvUDiN1P//Z",
+                            originalImg: "/static/b1cb549c6d70b9d75525fb6e7fb3ac08/06655/portrait.jpg",
+                            src: "/static/b1cb549c6d70b9d75525fb6e7fb3ac08/724c8/portrait.jpg",
+                            srcSet: "/static/b1cb549c6d70b9d75525fb6e7fb3ac08/84d81/portrait.jpg 250w,\n/static/b1cb549c6d70b9d75525fb6e7fb3ac08/f0719/portrait.jpg 500w,\n/static/b1cb549c6d70b9d75525fb6e7fb3ac08/724c8/portrait.jpg 1000w,\n/static/b1cb549c6d70b9d75525fb6e7fb3ac08/d79bd/portrait.jpg 1500w,\n/static/b1cb549c6d70b9d75525fb6e7fb3ac08/06655/portrait.jpg 1600w"
+                        }
+                    }
+                }
+            ]
+		}
 	};
 
 	it("renders data correctly", () => {
@@ -40,6 +60,6 @@ describe("Index Page", () => {
 
     it("matches the snapshot", () => {
         const { container } = render(<IndexPage data={data} />);
-        expect(container).toMatchSnapShot();
+        expect(container).toMatchSnapshot();
     });
 });
